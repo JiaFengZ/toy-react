95e0a4e94cea6f19606e925fa2f880e46862345f
unknown <1040185058@qq.com>
2020-09-14 23:43:35 +0800

    state and rerender

--- src/toy-react.js
+++ src/toy-react.js
@@ -1,18 +1,40 @@
+const RENDER_TO_DOM = Symbol('render ro dom')
+
 class ElementWrapper {
   constructor (tagName) {
     this.root = document.createElement(tagName)
   }
   setAttribute (name, value) {
-    this.root.setAttribute(name, value)
+    if (name.match(/^on([\s\S]+)$/)) {
+      this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLocaleLowerCase()), value)
+    } else {
+      if (name === 'className') {
+        this.root.setAttribute('class', value)
+      } else {
+        this.root.setAttribute(name, value)
+      }
+    }
   }
   appendChild (component) {
-    this.root.appendChild(component.root)
+    let range = document.createRange()
+    range.setStart(this.root, this.root.childNodes.length)
+    range.setEnd(this.root, this.root.childNodes.length)
+    range.deleteContents()
+    component[RENDER_TO_DOM](range)
+  }
+  [RENDER_TO_DOM] (range) {
+    range.deleteContents()
+    range.insertNode(this.root)
   }
 }
 
 class TextWrapper {
   constructor (text) {
     this.root = document.createTextNode(text)
+  }
+  [RENDER_TO_DOM] (range) {
+    range.deleteContents()
+    range.insertNode(this.root)
   }
 }
 
@@ -21,6 +43,7 @@ export class Component {
     this.props = Object.create(null)
     this.children = []
     this._root = null
+    this._range = null
   }
 
   setAttribute (name, value) {
@@ -31,12 +54,47 @@ export class Component {
     this.children.push(component)
   }
 
-  get root () {
-    if (!this._root) {
-      this._root = this.render().root
-    }
-    return this._root
+  [RENDER_TO_DOM] (range) {
+    this._range = range
+    this.render()[RENDER_TO_DOM](this._range)
   }
+
+  rerender () {
+    // this._range.deleteContents()
+    // this[RENDER_TO_DOM](this._range)
+
+    let oldRange = this._range
+
+    let range = document.createRange()
+    range.setStart(oldRange.startContainer, oldRange.startOffset)
+    range.setEnd(oldRange.startContainer, oldRange.startOffset)
+    this[RENDER_TO_DOM](range)
+
+    oldRange.setStart(range.endContainer, range.endOffset)
+    oldRange.deleteContents()
+  }
+
+  setState (newState) {
+    if (this.state === null || typeof this.state !== 'object') {
+      this.state = newState
+      this.rerender()
+      return
+    }
+
+    let merge = (oldState, newState) => {
+      for (let p in newState) {
+        if (oldState[p] === null || typeof oldState[p] !== 'object') {
+          oldState[p] = newState[p]
+        } else {
+          merge(oldState[p], newState[p])
+        }
+      }
+    }
+
+    merge(this.state, newState)
+    this.rerender()
+  }
+
 }
 
 export function createElement (type, attributes, ...children) {
@@ -54,6 +112,9 @@ export function createElement (type, attributes, ...children) {
       if (typeof child === 'string') {
         child = new TextWrapper(child)
       }
+      if (child === null) {
+        continue
+      }
       if (typeof child === 'object' && child instanceof Array) {
         insertChild(child)
       } else {
@@ -66,7 +127,11 @@ export function createElement (type, attributes, ...children) {
 }
 
 export function render (component, parentElement) {
-  parentElement.appendChild(component.root)
+  let range = document.createRange()
+  range.setStart(parentElement, 0)
+  range.setEnd(parentElement, parentElement.childNodes.length)
+  range.deleteContents()
+  component[RENDER_TO_DOM](range)
 }
 
 const ToyReact = {
